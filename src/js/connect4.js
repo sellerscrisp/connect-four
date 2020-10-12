aec2802079f0e561186c68221fd5cc2f63432ebe
Sellers Crisp <sellershcrisp@gmail.com>
2020-10-12 17:14:39 -0500

    unnecessary piece images

--- src/js/connect4.js
+++ src/js/connect4.js
@@ -5,82 +5,113 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
+const msgContainer = document.getElementById('msg-container');
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
- *    board = array of rows, each row is array of cells  (board[y][x])
+ *   board = array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  for (let y = 0; y < HEIGHT; y++) {
+    board.push(Array.from({ length: WIDTH }));
+  }
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+  const board = document.getElementById('board');
 
-  // TODO: add comment for this code
-  var top = document.createElement('tr');
+  // create clickable top element where game pieces are 'dropped'
+  const top = document.createElement('tr');
   top.setAttribute('id', 'column-top');
   top.addEventListener('click', handleClick);
 
-  for (var x = 0; x < WIDTH; x++) {
-    var headCell = document.createElement('td');
+  for (let x = 0; x < WIDTH; x++) {
+    const headCell = document.createElement('td');
     headCell.setAttribute('id', x);
     top.append(headCell);
   }
-  htmlBoard.append(top);
 
-  // TODO: add comment for this code
-  for (var y = 0; y < HEIGHT; y++) {
+  board.append(top);
+
+  // create game board cells
+  for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement('tr');
-    for (var x = 0; x < WIDTH; x++) {
+
+    for (let x = 0; x < WIDTH; x++) {
       const cell = document.createElement('td');
       cell.setAttribute('id', `${y}-${x}`);
+
       row.append(cell);
     }
-    htmlBoard.append(row);
+
+    board.append(row);
   }
 }
 
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 
 function findSpotForCol(x) {
-  // TODO: write the real version of this, rather than always returning 0
-  return 0;
+  for (let y = HEIGHT - 1; y >= 0; y--) {
+    if (!board[y][x]) {
+      return y;
+    }
+  }
+  return null;
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
-  // TODO: make a div and insert into correct table cell
+  const piece = document.createElement('div');
+  piece.classList.add('piece');
+  piece.classList.add('color' + `${currPlayer}`);
+  piece.style.top = -50 * (y + 2);
+
+  const spot = document.getElementById(`${y}-${x}`);
+  spot.append(piece);
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
-  // TODO: pop up alert message
+  msgContainer.classList.add('msg-won');
+  msgContainer.style.marginBottom = '-75px';
+  // msgContainer.classList.add('animated__animated animate__slideInLeft');
+  msgContainer.innerText = msg;
+  // msgContainer.style.setProperty('--animate-duration', '1s');
 }
+
+/** resetGame() */
+function resetGame() {
+  location.reload();
+}
+
+function restart() {
+  const resetBtn = document.querySelector('button');
+  resetBtn.addEventListener('click', resetGame());
+}
+
+// resetBtn.addEventListener('click', resetGame());
 
 /** handleClick: handle click of column top to play piece */
 
 function handleClick(evt) {
   // get x from ID of clicked cell
-  var x = +evt.target.id;
+  const x = +evt.target.id;
 
   // get next spot in column (if none, ignore click)
-  var y = findSpotForCol(x);
-  if (y === null) {
-    return;
-  }
+  const y = findSpotForCol(x);
+  if (y === null) return;
 
   // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
+  board[y][x] = currPlayer;
   placeInTable(y, x);
 
   // check for win
@@ -89,10 +120,21 @@ function handleClick(evt) {
   }
 
   // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
+  if (checkForTie()) endGame(`It's a tie :| `);
 
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
+  currPlayer = currPlayer === 1 ? 2 : 1;
+}
+/** checkForTie */
+function checkForTie() {
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
+      if (board[y][x] === undefined) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -113,35 +155,35 @@ function checkForWin() {
     );
   }
 
-  // TODO: read and understand this code. Add comments to help you.
-
-  for (var y = 0; y < HEIGHT; y++) {
-    for (var x = 0; x < WIDTH; x++) {
-      var horiz = [
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
+      // check each of the possible ways to win (4 in a row)
+      const horiz = [
         [y, x],
         [y, x + 1],
         [y, x + 2],
         [y, x + 3],
       ];
-      var vert = [
+      const vert = [
         [y, x],
         [y + 1, x],
         [y + 2, x],
         [y + 3, x],
       ];
-      var diagDR = [
+      const diagDR = [
         [y, x],
         [y + 1, x + 1],
         [y + 2, x + 2],
         [y + 3, x + 3],
       ];
-      var diagDL = [
+      const diagDL = [
         [y, x],
         [y + 1, x - 1],
         [y + 2, x - 2],
         [y + 3, x - 3],
       ];
 
+      // determine winner
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
       }
---
+++ src/js/modal.js
@@ -0,0 +1,1 @@
+// TODO: instead of using innerText, create a modal pop up with score and options to reset game!
